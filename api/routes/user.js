2caaa933414d54871f8fb2996bc110c0582534c5
hello <piash599@gmail.com>
2018-09-14 15:51:16 +0600

    added token to signin

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-router.post('/singup', (req, res, next) => {
+router.post('/signup', (req, res, next) => {
 
     User.find({
         email: req.body.email
@@ -43,6 +44,53 @@ router.post('/singup', (req, res, next) => {
     }).catch();
 });
 
+router.post('/login', (req, res, next) => {
+    User.find({
+            email: req.body.email
+        })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+
+                if (result) {
+
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    }, process.env.JWT_KEY, {
+                        expiresIn: '1h'
+                    })
+
+                    return res.status(200).json({
+                        message: 'Auth successfull',
+                        token: token
+                    });
+                }
+
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+})
+
 router.delete('/:userId', (req, res, next) => {
     User.remove({
             _id: req.body.userId
@@ -50,7 +98,7 @@ router.delete('/:userId', (req, res, next) => {
         .exec()
         .then(result => {
             return res.status(200).json({
-                message:'User deleted'
+                message: 'User deleted'
             });
         })
         .catch(err => {
